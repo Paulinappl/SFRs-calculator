65b3187cee04582437451b4a3fccc8641b7bf13f
Paulina Padilla López <139507739+Paulinappl@users.noreply.github.com>
2024-07-15 22:56:51 -0600

    Add files via upload
    First version input

---
+++ first_version_input.py
@@ -0,0 +1,29 @@
+# TOMAR UGRZ
+# TASAS SFR EN EL OPTICO Y UV 
+# VER CON QUE PARAMETROS DE ILUSTRIS SALE BIEN
+# QUEDARNOS CON 11 HALOS (CUMULOS DE GALAXIAS) 
+# SUB-HALOS = GALAXIAS
+
+# USAR EL COLOR G - R Y MASA, SI HAY MAS PUES MAS
+# MATRICES DE CORRELACIÓN
+
+
+# Luis and Paulina are going to work today on writing the code to take user input,
+# and Jiani and I are going to work on getting the TNG data and processing it to a 
+# form the model can use. If you want to work on any of that, let us know! Tomorrow 
+# we can actually work on the ML model itself and on predicting the SFRs from the user photometry
+
+import pandas as pd
+import numpy as np
+import csv
+import matplotli.pyplot as plt
+
+
+data = pd.read_csv("your_file.csv")
+
+
+
+
+
+
+
